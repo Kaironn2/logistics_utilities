3f3965c23b61a28421f92eaccdcfd8ffa133d0b7
Jonathas <125938287+Kaironn2@users.noreply.github.com>
2024-11-28 02:02:59 -0300

    update mgt_reader

--- mgt_reader.py
+++ mgt_reader.py
@@ -3,7 +3,6 @@ import xml.etree.ElementTree as ET
 from openpyxl import load_workbook
 from openpyxl.styles import Border, Side, NamedStyle, Alignment
 
-
 def magento_reader(magento_xml):
     namespaces = {'ss': 'urn:schemas-microsoft-com:office:spreadsheet'}
     tree = ET.parse(magento_xml)
@@ -34,8 +33,8 @@ def float_column_formater(df, coluns):
         df[column] = df[column].astype(float)
 
 
-magento_file_name = '09-setembro'
-magento_path = f'C:\\Users\\Jonh\\Desktop\\sl_codes\\1-magento\\raw\\{magento_file_name}' + '.xml'
+magento_file_name = '08-agosto'
+magento_path = f'C:\\Users\\jonat\\Desktop\\sl_codes\\{magento_file_name}' + '.xml'
 df = magento_reader(magento_path)
 
 
@@ -51,17 +50,18 @@ float_column_formater(new_df, magento_float_coluns)
 
 new_df = new_df.rename(columns={'Pedido #': 'OC'})
 
-new_df['Comprado Em'] = new_df['Comprado Em'].apply(lambda x: x[:10])  # Pegando só a data
+new_df['Comprado Em'] = new_df['Comprado Em'].apply(lambda x: x[:10])
 new_df['Comprado Em'] = new_df['Comprado Em'].apply(date_column_formater)
 new_df['OC'] = pd.to_numeric(new_df['OC'], errors='coerce')
 
 
 # Estilização dos dados na planilha site
-site_sheet_path = 'C:\\Users\\Jonh\\Desktop\\sl_codes\\site.xlsx'
+site_sheet_path = 'C:\\Users\\jonat\\Desktop\\sl_codes\\site.xlsx'
 wb = load_workbook(site_sheet_path)
 ws = wb['Magento']
 last_row = ws.max_row + 1
-
+ 
+# Estilização das células
 center_alignment = Alignment(horizontal='center')
 
 border = Border(
@@ -71,29 +71,46 @@ border = Border(
     right=Side(style='thin')
 )
 
+
+# Named Styles
 date_style = NamedStyle(
     name='date_style', 
     number_format="DD/MM/YYYY", 
-    border=border,
     )
 
-if 'date_style' not in wb.style_names:
-    wb.add_named_style(date_style)
+br_currency_style = NamedStyle(
+    name='br_currency_style',
+    number_format='R$0.00',
+)
+
+my_named_styles = [date_style, br_currency_style]
+
+for style in my_named_styles:
+    if style.name not in wb.style_names:
+        wb.add_named_style(style)
+
+
+date_columns = [2]
+currencies_columns = [7, 8 , 9]
 
 for r_index, row in new_df.iterrows():
-    for c_index, value in enumerate(row, 1):  # Enumerate começa de 1 para as colunas
+    for c_index, value in enumerate(row, 1):
+
         cell = ws.cell(row=last_row + r_index, column=c_index, value=value)
+        
+        if c_index in currencies_columns:
+            cell.style = 'br_currency_style'
+        
+        elif c_index in date_columns:
+            cell.style = 'date_style'
+
         cell.border = border
         cell.alignment=center_alignment
-
-        if c_index == 2:
-            cell.style = 'date_style'
-            cell.alignment=center_alignment
-
+            
 wb.save(site_sheet_path)
 
 print('Adição e estilização de dados em site.xlsx "magento" concluída!')
 
-mgt_export_name = ''
-mgt_export_path = f'C:\\Users\\Jonh\\Desktop\\sl_codes\\1-magento\\clean\\{mgt_export_name}' + '.xlsx'
+mgt_export_name = f'{magento_file_name} teste'
+mgt_export_path = f'C:\\Users\\jonat\\Desktop\\sl_codes\\export{mgt_export_name}' + '.xlsx'
 new_df.to_excel(mgt_export_path, index=False)
