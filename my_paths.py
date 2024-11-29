b7c0235482b36cde83e7d92e688a94b40dcfcaa6
Kaironn3 <jonhlojasanguelatino@gmail.com>
2024-11-29 17:34:21 -0300

    adding path localization

---
+++ my_paths.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+script_path = Path(__file__).resolve()
+
+logistic_utilities_path = script_path.parent
+
+sl_codes_path = logistic_utilities_path.parent
+
+ecs_path = sl_codes_path.joinpath('ecs.csv')
+mgt_path = sl_codes_path.joinpath('mgt.xml')
+site_sheet_path = sl_codes_path.joinpath('site.xlsx')
